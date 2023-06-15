c14c4d241d1a570329cdb17f8d9d05205c3dbb70
OhJaeYoung <ostyoung@naver.com>
2023-06-15 22:55:07 +0900

    0615

---
+++ C++/StepFour/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void _10807()
+{
+	int N = 0;
+	int num[100] = {};
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num[i];
+	}
+
+}
+
+int main()
+{
+	_10807();
+	return 0;
+}
